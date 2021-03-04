a890e086dbfd7a561c9ba3ead9c97785622b8fbd
Lorenalgm <lorena@proesc.com>
2021-03-03 23:53:49 -0300

    Atualizado teste com verificação da rota inicial

--- src/__tests__/invoice.spec.js
+++ src/__tests__/invoice.spec.js
@@ -1,5 +1,14 @@
+const app = require('../server');
+
 describe("Testing the invoice API", () => {
-    
+  it("tests the base route and returns true for status", async () => {
+
+		const response = await supertest(app).get('/');
+		expect(response.status).toBe(200);
+		expect(response.body.status).toBe(true);
+
+  });
+  
   it("tests the get invoice endpoint and have have message property", () => {
     expect(200).toBe(200);
   });
@@ -8,7 +17,7 @@ describe("Testing the invoice API", () => {
     expect(200).toBe(200);
   });
 
-  it("tests the put endpoint and returns as success message", () => {
+  it("tests the put endpoint and returns and returns as success message", () => {
     expect(200).toBe(200);
   });
 
