a35de979a6951bf4dbed456c471a5a58ea3fa59e
Lorenalgm <lorena@proesc.com>
2021-03-04 00:08:19 -0300

    Criadas rotas para invoice

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const InvoiceController = require('./controllers/InvoiceController');
 
 const routes = express.Router();
 
 app.get('/', (req, res) => {
 	res.json({ status: true })
 });
+
+routes.get('/invoices', InvoiceController);
+routes.post('/invoices', InvoiceController);
+routes.put('/invoices', InvoiceController);
+routes.patch('/invoices', InvoiceController);
+routes.delete('/invoices', InvoiceController);
 
 module.exports = routes;
