95c4e2864c8b8b1b99a11bf62c59c2465b561cc9
Lorenalgm <lorena@proesc.com>
2021-03-03 23:35:50 -0300

    Criados testes para os 5 endpoints

---
+++ src/__tests__/invoice.js
@@ -0,0 +1,24 @@
+describe("Testing the invoice API", () => {
+    
+  it("tests the get invoice endpoint and have have message property", () => {
+    expect(200).toBe(200);
+  });
+
+  it("tests the post new invoice endpoint returns as success message", () => {
+    expect(200).toBe(200);
+  });
+
+  it("tests the put endpoint and returns as success message", () => {
+    expect(200).toBe(200);
+  });
+
+  it("tests the patch invoice endpoint and returns as success message", () => {
+    expect(200).toBe(200);
+  });
+
+  it("tests the delete invoice endpoint and returns as success message", () => {
+    expect(200).toBe(200);
+  });
+
+
+});
