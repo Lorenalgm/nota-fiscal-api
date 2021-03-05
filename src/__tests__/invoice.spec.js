2b1ff3d49d93855b5a7f651a10f87abb70f3703b
Lorenalgm <lorena@proesc.com>
2021-03-04 23:27:42 -0300

    Refatorados testes de nota fiscal

--- src/__tests__/invoice.spec.js
+++ src/__tests__/invoice.spec.js
@@ -1,33 +1,61 @@
+const supertest = require('supertest');
+const Invoice = require('../models/Invoice');
 const app = require('../index');
 
 describe("Testing the invoice API", () => {
-  it("tests the base route and returns true for status", async () => {
-
+  it("tests the base route and returns true for status", async done => {
 		const response = await supertest(app).get('/');
 		expect(response.status).toBe(200);
 		expect(response.body.status).toBe(true);
-
+    done();
   });
   
-  it("tests the get invoice endpoint and have have message property", () => {
-    expect(200).toBe(200);
+  it("tests the get invoice endpoint and have have message property", async done => {
+		const response = await supertest(app).get('/invoices');
+		expect(response.status).toBe(200);
+    done();
   });
 
-  it("tests the post new invoice endpoint returns as success message", () => {
+  it("tests the post new invoice endpoint returns as success message", async done => {
+    const response = await supertest(app).post('/invoices').send({
+			ReferenceMonth: 2,
+			ReferenceYear: 2021,
+			Description: 'Nota fiscal reference a mensalidade da escola',
+			Amount: 200.5
+    });
     expect(200).toBe(200);
+    done();
   });
 
-  it("tests the put endpoint and returns and returns as success message", () => {
+  it("tests the put endpoint and returns and returns as success message", async done => {
+    const response = await supertest(app).put('/invoices').send({
+			ReferenceMonth: 3,
+			ReferenceYear: 2021,
+			Description: 'Nota fiscal reference a mensalidade da escola',
+			Amount: 240.5
+    });
     expect(200).toBe(200);
+    done();
   });
 
-  it("tests the patch invoice endpoint and returns as success message", () => {
+  it("tests the patch invoice endpoint and returns as success message", async done => {
+    const response = await supertest(app).patch('/invoices').send({
+			ReferenceMonth: 2,
+    });
     expect(200).toBe(200);
+    done();
   });
 
-  it("tests the delete invoice endpoint and returns as success message", () => {
+  it("tests the delete invoice endpoint and returns as success message", async done => {
+    const response = await supertest(app).delete('/invoices');
     expect(200).toBe(200);
+    done();
   });
 
+  afterEach(async () => {
+		await Invoice.deleteOne({
+			Description: 'Nota fiscal reference a mensalidade da escola'
+		})
+	})
 
 });
